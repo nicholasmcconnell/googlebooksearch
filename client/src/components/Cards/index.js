eac05d8eb6ca38bb6149f9fd320b9824c8c9ce63
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-05-19 11:08:31 -0400

    UI styling applied

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -6,18 +6,21 @@ function Cards(props) {
 
     return (
         <div>
-            <div className="card offset-1 col-md-5" style={{display: "flex"}}>
+            <div className="card offset-1 col-md-5" style={{ display: "flex" }}>
                 <img src={image} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h4 className="card-title">{props.book.title}</h4>
                     <h4 className="card-title">{props.book.authors.join(", ")}</h4>
                     {/* <div data-spy="scroll" data-target="#navbar-example2" data-offset="0"> */}
-                        <p className="crop description">{props.book.description}</p>
+                    <p className="crop description">{props.book.description}</p>
 
                     {/* </div> */}
                     {/* <div className="buttonDiv text-center d-flex justify-content-center"> */}
-                    <a onClick={!props.saveView ? () => props.saveBook(props.book) : () => props.deleteBook(props.book._id)} className="btn btn-primary">{!props.saveView ? "Save" : "Delete"}</a>
-                    <a href="#" target="_blank" href={props.book.infoLink} className="btn btn-primary">More</a>
+
+                    <div className="buttons">
+                        <a onClick={!props.saveView ? () => props.saveBook(props.book) : () => props.deleteBook(props.book._id)} className="btnSaveDelete btn btn-primary">{!props.saveView ? "Save" : "Delete"}</a>
+                        <a href="#" target="_blank" href={props.book.infoLink} className="btn btn-primary">More</a>
+                    </div>
                     {/* </div> */}
                 </div>
             </div>
--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, width: "60%", clear: "both", paddingTop: 120, textAlign: "center", marginTop: "50px", marginLeft: "20%"}}
+      style={{ height: 200, width: "60%", clear: "both", paddingTop: 75, textAlign: "center", marginTop: "50px", marginLeft: "20%"}}
       className="jumbotron"
     >
       {children}
